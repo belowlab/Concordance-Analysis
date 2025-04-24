ac41f73c3a81ad9c9e8a5d38b06b5e84c8282fc8
alyssascartozzi <71240510+alyssascartozzi@users.noreply.github.com>
2025-04-24 15:49:11 -0500

    Update concordance.py

--- concordance.py
+++ concordance.py
@@ -1,7 +1,5 @@
 import sys
 ###quick note, script will only work in python3. Assumes ordered dictionary
-file1_dic = {}
-file2_dic = {}
 
 sim1 = {} # dic[rsid] = [+/-,1/0] #1 indicates new block
 sim2 = {}
@@ -9,7 +7,7 @@ sim2 = {}
 file1 = open(sys.argv[1],"r")
 file2 = open(sys.argv[2],"r")
 
-pval_thresh = 0.005 #### CHANGE TO THE PVALUE THRESHOLDS AS NEEDED
+pval_thresh = float(sys.argv[3]) #change pvalue threshold as needed, used 0.005 for publication
 
 ###first summary statistics file
 file1.readline()
@@ -17,37 +15,43 @@ first = True
 print("reading file 1")
 for line in file1:
     spline = line.split()
-    if(float(spline[8].rstrip())<pval_thresh): ##### CHANGE spline[index] to the correct pvalue column
+    if(True):
         ###create sim dic
+        meets_pval = float(spline[8].rstrip())<pval_thresh ##### CHANGE spline[index] to the correct pvalue column
         if(first):
             pos = int(spline[2]) ####### CHANGE spline[index] to column with the basepair position
             if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
                 sign = "+"
-                sim1[spline[0]] = [sign,1] ##### CHANGE spline[inxdex to the SNP column
+                sim1[spline[0]] = [sign,1,meets_pval] ##### CHANGE spline[inxdex to the SNP column
             else:
                 sign = "-"
-                sim1[spline[0]] = [sign,1] ##### CHANGE spline[inxdex to the SNP column
+                sim1[spline[0]] = [sign,1,meets_pval] ##### CHANGE spline[inxdex to the SNP column
             first = False
         else:
             if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
                 new_sign = "+"
             else:
                 new_sign = "-"
-            if((int(spline[2])-int(pos))<10000 and sign == new_sign):#if still in ld block (same direction of effect and within 10kb ####### CHANGE spline[index] to column with the basepair position
-                sim1[spline[0]] = [sign,"0"] ##### CHANGE spline[inxdex] to the SNP column
+            if((int(spline[2])-int(pos))<10000 and sign == new_sign): ##if still in ld block (same direction of effect and within 10kb ####### CHANGE spline[index] to column with the basepair position
+                sim1[spline[0]] = [sign,0,meets_pval] ##### CHANGE spline[inxdex] to the SNP column
                 pos = spline[2] #update position ####### CHANGE spline[index] to column with the basepair position
             else:#if new ld block
-                sim1[spline[0]] = [new_sign,"1"] #####AS: CHANGE spline[inxdex] to the SNP column
-                sign = new_sign #update to the new sign
+                sim1[spline[0]] = [new_sign,1,meets_pval] ###CHANGE spline[inxdex] to the SNP column
+                sign = new_sign ##update to the new sign
                 pos = spline[2] #update position  ####### CHANGE spline[index] to column with the basepair position
 
-
-        ###create true dictionary
-        if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
-            file1_dic[spline[0]] = "+" ##### CHANGE spline[index] to the SNP column
-        else:
-            file1_dic[spline[0]] = "-" ##### CHANGE spline[index] to the SNP column
-
+###Prune dictionary and update blocks
+print("Pruning dictionary 1...")
+update_next = False
+for rsid in list(sim1.keys()):
+    if(sim1[rsid][2]==False):
+        if(sim1[rsid][1]==1):
+            update_next = True
+        del sim1[rsid]
+    else:
+        if(update_next):
+            sim1[rsid][1]=1 ###if the start of a block gets removed, update the next position to become the new start of the block
+        update_next = False
 
 ###second summary statistics file
 file2.readline()
@@ -55,42 +59,62 @@ first = True
 print("reading file 2")
 for line in file2:
     spline = line.split()
-    if(float(spline[8].rstrip())<pval_thresh):  ##### CHANGE spline[index] to the correct pvalue column
+    if(True):
         ###create sim dictionary
+        meets_pval = float(spline[8].rstrip())<pval_thresh ##### CHANGE spline[index] to the correct pvalue column
         if(first):###First read just establishes the starting position and sign
             pos = int(spline[2]) ####### CHANGE spline[index] to column with the basepair position
-            if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
+            if(float(spline[6])>0):  ###### CHANGE spline[index] to the BETA column
                 sign = "+"
-                sim2[spline[0]] = [sign,1] ##### CHANGE spline[inxdex to the SNP column
+                sim2[spline[0]] = [sign,1,meets_pval] ##### CHANGE spline[inxdex to the SNP column
             else:
                 sign = "-"
-                sim2[spline[0]] = [sign,1] ##### CHANGE spline[inxdex to the SNP column
+                sim2[spline[0]] = [sign,1,meets_pval] ##### CHANGE spline[inxdex to the SNP column
             first = False
         else:
-            if(float(spline[6])>0):  ###### CHANGE spline[index] to the BETA column
+            if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
                 new_sign = "+"
             else:
                 new_sign = "-"
-            if((int(spline[2])-int(pos))<10000 and sign == new_sign):#if still in ld block (same direction of effect and within 10kb.. #######AS: CHANGE spline[index] to column with the basepair position
-                sim2[spline[0]] = [sign,"0"] ##### CHANGE spline[index] to the SNP column
+            if((int(spline[2])-int(pos))<10000 and sign == new_sign): #if still in ld block (same direction of effect and within 10kb.. #######AS: CHANGE spline[index] to column with the basepair position
+                sim2[spline[0]] = [sign,0,meets_pval] ##### CHANGE spline[index] to the SNP column
                 pos = spline[2] #update position  ####### CHANGE spline[index] to column with the basepair position
-            else:#if new ld block
-                sim2[spline[0]] = [new_sign,"1"] ##### CHANGE spline[inxdex to the SNP column
+            else: #if new ld block
+                sim2[spline[0]] = [new_sign,1,meets_pval] ##### CHANGE spline[inxdex to the SNP column
                 sign = new_sign #update to the new sign
                 pos = spline[2] #update position ####### CHANGE spline[index] to column with the basepair position
 
 
+print("Pruning dictionary 2...")
+update_next = False
+for rsid in list(sim2.keys()): 
+    if(sim2[rsid][2]==False): #check if SNP does not meet the p-value threshold
+        if(sim2[rsid][1]==1): #check if it's the start of an LD block
+            update_next = True #mark to update the next SNP as the new start of the block
+        del sim2[rsid] #delete the SNP from the dictionary
+    else:
+        if(update_next): 
+            sim2[rsid][1]=1 ###if the start of a block gets removed, update the next position to become the new start of the block
+        update_next = False #reset the marker
 
+###calculate the concordance before we start shuffling the dictionaries
+real_total = 0
+real_overlap = 0
 
-        ###create true dictionary
-        if(float(spline[6])>0): ###### CHANGE spline[index] to the BETA column
-            file2_dic[spline[0]] = "+" ##### CHANGE spline[index] to the SNP column
-        else:
-            file2_dic[spline[0]] = "-" ##### CHANGE spline[index]to the SNP column
-
+for rsid in sim1:
+    if(sim1[rsid][1]==1): #check if it's the start of an LD block in sim1
+        if(rsid in sim2): #check if the rsid also exists in sim2
+            real_total+=1 #increase total
+            if(sim1[rsid][0]==sim2[rsid][0]): #check if the sign of effect matches
+                real_overlap+=1 #increase total
+    else: #if not the start of an LD block
+        if(rsid in sim2): #check if the rsid exists in sim2
+            if(sim2[rsid][1]==1): #check if it's the start of an LD block in sim2
+                real_total+=1 #increase total
+                if(sim1[rsid][0]==sim2[rsid][0]):
+                    real_overlap+=1 #increase total
 
 ###simulate concordance analysis to establish an expected percentage of overlap
-###simulation randomly shuffles the direction of effects of entire blocks (blocks defined by variants that are within 10kb and have the same direction of effect) for both ancestry groups
 sim = 25 #run the concordance analysis x times 
 conc_rates = []
 import random
@@ -99,7 +123,7 @@ for i in range(0,sim):
     ###randomize the dictionaries
     ##rand dic 1
     for rsid in sim1:
-        if((sim1[rsid])[1]):###if its a new block... which the first one should be    
+        if((sim1[rsid])[1]): ###if its a new block
             if(random.randint(0,1)): ###flip a coin
                 rand_sign = "+"
             else:
@@ -117,37 +141,63 @@ for i in range(0,sim):
                 rand_sign = "-"
             (sim2[rsid])[0] = rand_sign #update dictionary with random sign
         else: ###if its not a new block...
-            (sim2[rsid])[0] = rand_sign #just update it to the previous randomly drawn sign
+            (sim2[rsid])[0] = rand_sign #just update it to the previous randomly drawn sigh
 
-    ###find concordance rate with randomized dictionaries
+    ###find concordance rate with randomized dictionaries, block by block
     total = 0
     overlap = 0
     for rsid in sim1:
-        if(rsid in sim2):
-            total+=1
-            if((sim1[rsid])[0]==(sim2[rsid])[0]):
-                overlap+=1
+        if(sim1[rsid][1]==1):
+            if(rsid in sim2):
+                total+=1
+                if(sim2[rsid][0]==sim1[rsid][0]):
+                    overlap+=1
+        else:
+            if(rsid in sim2):
+                if(sim2[rsid][1]==1):
+                    total+=1
+                    if(sim2[rsid][0]==sim1[rsid][0]):
+                        overlap+=1
+
     conc_rates.append(overlap/total)
 
-total = 0
-overlap = 0
-for rsid in file1_dic:
-    if(rsid in file2_dic):
-        total +=1
-        if(file1_dic[rsid] == file2_dic[rsid]):
-            overlap += 1
+
+
 print(conc_rates)
 expected_concordance = sum(conc_rates)/len(conc_rates)
 
 
-print("total:",total)
-print("overlap:",overlap)
-print("concordance:", overlap/total)
+print("total:",real_total)
+print("overlap:",real_overlap)
+print("concordance:", real_overlap/real_total)
 print("expected concordance rate based on simluation:", expected_concordance)
 print("pval threshold:",pval_thresh)
 
 from scipy import stats
-con_p = stats.binom_test(overlap, n=total, p=expected_concordance, alternative='greater')
+con_p = stats.binom_test(real_overlap, n=real_total, p=expected_concordance, alternative='greater')
+print("pvalue: ",con_p)
+
+
+f = open("23andMe_concordance.csv","a") #open results file
+f.write("\n")
+f.write(sys.argv[1])
+f.write(",")
+f.write(sys.argv[2])
+f.write(",")
+f.write(sys.argv[3])
+f.write(",")
+f.write(str(real_total))
+f.write(",")
+f.write(str(expected_concordance))
+f.write(",")
+f.write(str(real_overlap/real_total))
+f.write(",")
+f.write(str(con_p))
+f.close()
+
+
+file1.close()
+file2.close()
 print("pvalue: ",con_p)
 
 
